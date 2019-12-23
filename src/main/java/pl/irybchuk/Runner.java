6aa535f5e271d910afacabfc36e60c3be9c8958c
irybchuk <yrybchuk@gmail.com>
2019-12-23 19:58:11 +0100

    Improve RentVehicleQuery

--- src/main/java/pl/irybchuk/Runner.java
+++ src/main/java/pl/irybchuk/Runner.java
@@ -1,12 +1,9 @@
 package pl.irybchuk;
 
 import pl.irybchuk.model.*;
-import pl.irybchuk.service.Exception.UserNotAdultException;
 import pl.irybchuk.service.RentVehicleService;
-import pl.irybchuk.util.ConnectionUtil;
 
 import java.math.BigDecimal;
-import java.util.Date;
 
 public class Runner {
 
@@ -14,7 +11,7 @@ public class Runner {
         RentVehicleService rentVehicleService = new RentVehicleService(null);
 
         User user = new User(1,"Iaroslav","Rybchuk", null);
-        Vehicle vehicle = new Vehicle(TransportType.BICYCLE,1,"Tesla");
+        Vehicle vehicle = new Vehicle(TransportType.BICYCLE, 5, "Tesla");
         Address address = new Address(1,"Warsaw","Jana Pawla");
         Price price = new Price(1,PriceUnit.MINUTE, BigDecimal.valueOf(10),"USD");
 
--- src/main/java/pl/irybchuk/util/query/RentVehicleQuery.java
+++ src/main/java/pl/irybchuk/util/query/RentVehicleQuery.java
@@ -1,6 +1,5 @@
 package pl.irybchuk.util.query;
 
-import org.joda.time.DateTime;
 import pl.irybchuk.model.Address;
 import pl.irybchuk.model.Price;
 import pl.irybchuk.model.User;
@@ -11,28 +10,24 @@ import java.util.Map;
 public final class RentVehicleQuery {
 
     public static final String insertIntoQuery (Map<String, Object> queryParameters) {
+
         String query = "INSERT INTO ride_information"
-                + " (id_user, id_vehicle, id_startAddress, id_endAddress, start_time, end_time, id_price)"
-                + "Values (idUser, idVehicle, idStartAddress, idEndAddress, startTime, endTime, price)";
+                + " (id_user, id_vehicle, id_startAddress, start_time, id_price)"
+                + " Values (idUser, idVehicle, idStartAddress, startTime, idPrice)";
 
         User user = (User) queryParameters.get("idUser");
-        query.replace("idUser", String.valueOf(user.getId()));
+        query = query.replace("idUser", String.valueOf(user.getId()));
 
         Vehicle vehicle = (Vehicle) queryParameters.get("idVehicle");
-        query.replace("idVehicle", String.valueOf(vehicle.getId()));
+        query = query.replace("idVehicle", String.valueOf(vehicle.getId()));
 
-        //--------------------------------------------------------
         Address startAddress = (Address) queryParameters.get("idStartAddress");
-        query.replace("idStartAddress", String.valueOf(startAddress.getId()));
+        query = query.replace("idStartAddress", String.valueOf(startAddress.getId()));
 
-        query.replace("idEndAddress", "");
-
-        query.replace("startTime", String.valueOf(DateTime.now()));
-
-        query.replace("endTime", "");
+        query = query.replace("startTime", "'2019-09-18 13:45:23'");
 
         Price price = (Price) queryParameters.get("idPrice");
-        query.replace("idPrice",String.valueOf(price.getId()));
+        query = query.replace("idPrice", String.valueOf(price.getId()));
 
         return query;
     }
